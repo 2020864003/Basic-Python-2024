bc4c420492fd669e52ee57a697bd857798ea1d7b
Roy-Lee <ljunghwan928@naver.com>
2024-01-29 15:28:11 +0900

    upload

--- day01/test01_consol.py
+++ day01/test01_consol.py
@@ -3,3 +3,6 @@
 
 #콘솔 출력을 위해 print() 함수를 사용 -> 주석 : 소스 실행에 영향 주지 않음
 print('Hello, World!')
+
+# > python test01 console.py 로 콘솔 실행
+# VS code 에서 ctrl F5 -> 실행 디버깅 없이 실행
---
+++ day01/test03_datatype.py
@@ -0,0 +1,23 @@
+# date : 20240129
+# desc : 자료형
+
+## 기본자료형 - 숫자형, 문자형, 불형
+## 추가자료형 - 리스트형, 튜플형, 딕셔너리, 집합...
+
+## None형 == null(C, C++, C#, JAVA, SQL)
+## 값은 값인데 아무것도 지정할수 없는 값 -> NONE
+
+apple = None
+print(apple)
+
+## 숫자형 - 정수형, 실수형, 진수형
+ten = 10
+hundred = 100
+temp = -8
+
+#### 실수
+pi = 3.1415
+tax_rate = 10.0
+emp_earn_rate = 3.3
+test_val = 2e10
+print(test_val)
