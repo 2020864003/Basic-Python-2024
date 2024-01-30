ac6c9a582377b22929637be7e8454a3f908869f0
Roy-Lee <ljunghwan928@naver.com>
2024-01-30 09:47:55 +0900

    update

--- day02/test1_flow.py
+++ day02/test1_flow.py
@@ -2,7 +2,7 @@
 # desc : 흐름제어 if문자
 
 ## 조건이 참, 거짓일때 나누어서 어떠한 일처리 if
-number = int(input())
+number = int(input('숫지 입력 >'))
 ## 입력 함수 input() = 문자
 ## int(input()) - 문자로 된 입력밧을 정수로 변겅
 
@@ -12,5 +12,8 @@ if number > 0 : ## if 조건:  참인 조건
 elif number == 0 : ## 양,음수 x
     print('0입니다~')
 
-else : ## else: 거짓인 조건
+elif number < 0:
     print('negative')
+
+else : ## else: 거짓인 조건
+    print('정의 할수가 없습니다.')
