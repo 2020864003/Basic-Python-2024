babf8f71cc01d3379537ffdbf460bd8db3b1e1fa
Roy-Lee <ljunghwan928@naver.com>
2024-01-31 17:16:48 +0900

    update

---
+++ day03/test23_module.py
@@ -0,0 +1,21 @@
+# date : 20240131
+# file : test23_module.py
+# desc : 모듈사용하기
+import math
+
+PI = 3.141592
+radius = 5
+print(f'원의 크기는 {radius * radius * PI}')
+
+## print(math.pi)
+print(f'정확한 원의 크기는 {radius * radius * math.pi}')
+
+print(f'cos(0) = {math.cos(0)}')
+print(2 ** 10)
+print(math.pow(2, 10))
+print(math.ceil(3.1))
+print(math.floor(3.5))
+print(round(3.5)) # 반올림(너무 사용하니까 MATH에 없음. 기본함수)
+
+import math as m # 별명을 짓기
+print(m.sin(2))
