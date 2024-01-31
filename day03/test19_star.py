abb8135f0a8802da0e1c38ea45d03921ae848eb9
Roy-Lee <ljunghwan928@naver.com>
2024-01-31 11:41:51 +0900

    Update

--- day03/test19_star.py
+++ day03/test19_star.py
@@ -13,5 +13,8 @@ for i in range(1, 5 + 1):
     # i 가 2이면, range(1,3) 2번 반복
 
     for j in range(1, 6 - i + 1) :  # range() 값 바꿔서 디버깅
-        print('*', end='') # enter 대신 empty
+        print(' ', end='') # enter 대신 empty
+    
+    for j in range(1, i + 1):
+        print('*', end='')
     print('') # 안쪽 for 문이 끝나면 엔터
