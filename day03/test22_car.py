e593dc69fe308d93408b99ff22a5376f5f6ea836
Roy-Lee <ljunghwan928@naver.com>
2024-01-31 16:23:27 +0900

    update

--- day03/test22_car.py
+++ day03/test22_car.py
@@ -30,11 +30,25 @@ class Car:
            self.color = col
            self.geartype = gear
 
+    def __str__(self) -> str: # print(객체) 출력되는 부분
+          return f'제 차는 {self.plate_num} 입니다. {self.color} 입니다.'
+    
+    def getPlateNumber(self) : # 외부에서는 접근할 수 없도록 막는 조치
+          return self.plate_num
+    
+    def setPlateNumber(self, new_plateNuber):
+          self.plate_num = new_plateNuber
+
 tucson = Car('12가 3456', 'HYUMDAI', 'BLACK','AUTOMATIC')
+# 객체를 생성하는 곳과 객체를 사용하는 곳이 다를 수 있습니다.
+
 print(tucson)
-print(f'차번호는 {tucson.plate_num}')
+print(f'차번호는 {tucson.getPlateNumber}')
 print(f'색상은 {tucson.color}')
 tucson.moveForward()
 
-ioniq = Car('98가7654', 'KIA', 'WHITE', 'MANUAL')
-print(f'차번호는 {ioniq.plate_num}')
+# ioniq = Car('98가7654', 'KIA', 'WHITE', 'MANUAL')
+# print(f'차번호는 {ioniq.plate_num}')
+
+# tucson.plate_num = '123Ha 7896'
+# print(tucson)
