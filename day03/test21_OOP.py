7d28b0452fa5260dd6940524464b5beb04a21e79
Roy-Lee <ljunghwan928@naver.com>
2024-01-31 14:03:33 +0900

    main

--- day03/test21_OOP.py
+++ day03/test21_OOP.py
@@ -2,5 +2,15 @@
 # file : test21_OOP.py
 # desc : 클래스
 
-class Person :
+# Class (사람이라는 객체를 만들기 위한 청사진) 만들기
+class Person : # 사람 클래스 선언
     pass
+
+# 사람 객체 생성 -> instance(사례, 예제)
+roy = Person()
+lee = Person()
+
+print(type(roy))
+# print(type(3.14))
+print(id(roy))
+print(id(lee))
