46a469beff2a10a6ffa37e0e63b341fe86752321
Roy-Lee <ljunghwan928@naver.com>
2024-02-01 10:43:26 +0900

    Update

--- day04/test25_web.py
+++ day04/test25_web.py
@@ -11,3 +11,12 @@ res = urlopen(req) # url 주소로 웹사이트 열어달라고 요청
 
 print(f' 응답코드는: {res.status}') # URL로 요청된 웹사이트 응답 확인!
 print(res.read())
+
+
+# urllib.request 보다 성능이 조금 더 나은 모듈
+import requests
+
+res2 = requests.get('https://www.naver.com/')
+
+print(res2.status_code)
+print(res2.text)
