71dbebb091e7967c71c1f57c481f30861a8190eb
Roy-Lee <ljunghwan928@naver.com>
2024-02-05 15:43:20 +0900

    update

--- day06/test40_thread.py
+++ day06/test40_thread.py
@@ -22,7 +22,31 @@ class BackWorker(QThread): #PyQt에서 스레드 클래스 상속
         for i in range(maxVal): # 0 ~ 100
            print_str = f'No Thread 출력 > {i}'
            print(print_str)
-           self.txbLog.append(print_str)
-           self.pgbTask.setValue(i)
+           self.parent.txbLog.append(print_str)
+           self.parent.pgbTask.setValue(i)
     
-    
+
+class qtwin_exam(QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+        uic.loadUi('./day06/ThreadApp.ui',self) #QtDesinger에서 만든 UI를 load
+        # BUTTON에 대한 시그널 처리 
+        self.btnStart.clicked.connect(self.btnStartClicked) # UI 파일내에 있는 위젯접근은 VScode상에서 색상으로 표시 x
+        
+    def btnStartClicked(self):
+        th = BackWorker(self)
+        th.start() # BackWorker 내의 self.run() 실행
+
+    def closeEvent(self, QCloseEvent) -> None: # X버튼 종료확인
+        re = QMessageBox.question(self,'종료 확인','종료 하실?', QMessageBox.Yes|QMessageBox.No)
+        if re == QMessageBox.Yes: # 닫기
+            QCloseEvent.accept()
+        else:
+            QCloseEvent.ignore()
+
+
+if __name__ == '__main__':
+    loop = QApplication(sys.argv)
+    instance = qtwin_exam()
+    instance.show()
+    loop.exec_()
