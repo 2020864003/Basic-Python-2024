5d45e8cffe7b08f4f0757c4aa1ec10a648455a9c
Roy-Lee <ljunghwan928@naver.com>
2024-02-05 11:34:39 +0900

    update

--- day06/test37_pyqt5.py
+++ day06/test37_pyqt5.py
@@ -5,7 +5,7 @@
 import sys
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPainter, QColor, QFont
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox
 
 class qtwin_exam(QWidget):
     def __init__(self) -> None:
@@ -15,11 +15,16 @@ class qtwin_exam(QWidget):
     def initUI(self):
         btn01 = QPushButton('Click', self)
         btn01.setGeometry(50, 100, 100, 40)
-        btn01.clicked.connect(self.btn01_clicked) # 버튼 클릭하면 버튼01_클릭ed 함수 실행 시키겠다.
+        btn01.clicked.connect(self.btn1_clicked) # 버튼 클릭하면(Signal) 버튼01_클릭ed2 함수 실행 시키겠다.
 
         self.setGeometry(300, 200, 400, 200)
         self.setWindowTitle('Button Event!!')
         self.show()
+
+    def btn1_clicked(self):
+        QMessageBox.about(self, 'Button Click', 'You Clicked Button!!')
+
+
 
 
 loop = QApplication(sys.argv)
