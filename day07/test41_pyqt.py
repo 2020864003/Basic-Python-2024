e06721baa161e03a4c3e234f7fe0ef68a8aa6bd4
Roy-Lee <ljunghwan928@naver.com>
2024-02-06 12:04:45 +0900

    update

---
+++ day07/test41_pyqt.py
@@ -0,0 +1,37 @@
+# date : 20240206
+# file : test41_pyqt.py
+# desc : 이미지 뷰어
+
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QWidget
+
+class winApp(QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+        self.initUI() 
+
+    def initUI(self):
+        
+        lblSize = QLabel(self)
+        lblSize.setText('Sample')
+        lblSize.setSizeIncrement(200,50)
+        lblSize.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+
+        self.setWindowTitle('Image Viewer')
+        self.setGeometry(300, 300, 300, 300)
+        
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    screen_react = app.desktop().screenGeometry()
+    width, height = screen_react.width(), screen_react.height()
+    print(width, 'x', height)
+    inst = winApp()
+    inst.show()
+    sys.exit(app.exec_()) # 종료시 리소스 변환등 효율
