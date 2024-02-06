0fa4d4f886bc991b052ddc1f3e161a78d36cf699
Roy-Lee <ljunghwan928@naver.com>
2024-02-06 14:00:14 +0900

    update

--- day07/test41_pyqt.py
+++ day07/test41_pyqt.py
@@ -21,7 +21,10 @@ class winApp(QWidget):
         lblImage.setPixmap(pixmap)
 
         lblSize = QLabel(self)
+        lblSize.setFont(QFont('NanumGothicCoding', 15)) # Font + Font Size
+        lblSize.setStyleSheet('Color: Blue')
         lblSize.setText(f'{pixmap.width()}x{pixmap.height()}')
+        lblSize.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter) # 가로중앙정렬 | 세로 중앙정렬
         # kitty.jpg의 width heigth
 
         vbox = QVBoxLayout(self) # QtDesigner VerticalLayout 위젯 생성
