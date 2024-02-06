4378b952a6a4d0f13cbca9fe10202d32fdf2fb11
Roy-Lee <ljunghwan928@naver.com>
2024-02-06 12:07:31 +0900

    업데이트

--- day07/test41_pyqt.py
+++ day07/test41_pyqt.py
@@ -15,12 +15,12 @@ class winApp(QWidget):
         self.initUI() 
 
     def initUI(self):
-        
         lblSize = QLabel(self)
         lblSize.setText('Sample')
         lblSize.setSizeIncrement(200,50)
         lblSize.setAlignment(Qt.AlignmentFlag.AlignHCenter)
 
+        self.setWindowIcon(QIcon('./image/iot.png'))
         self.setWindowTitle('Image Viewer')
         self.setGeometry(300, 300, 300, 300)
         
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     screen_react = app.desktop().screenGeometry()
     width, height = screen_react.width(), screen_react.height()
-    print(width, 'x', height)
+    print(width, 'x', height) # 활용도 높음
     inst = winApp()
     inst.show()
     sys.exit(app.exec_()) # 종료시 리소스 변환등 효율
